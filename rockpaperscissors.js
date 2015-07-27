f4e9083dda304719f56d6272804e58ad34165d11
mformanish <manish.gupta433@gmail.com>
2015-07-27 16:25:09 +0530

    Create rockpaperscissors.js

---
+++ rockpaperscissors.js
@@ -0,0 +1,41 @@
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} 
+
+console.log("Computer: " + computerChoice);
+
+var compare = function(choice1,choice2){
+    if(choice1 === choice2){
+        return "The result is a tie!";
+    }
+    else if(choice1 === "rock") {
+        if(choice2 === 'scissors')
+        return "Rock Wins"
+        else if(choice2 === "paper")
+        return "Paper Wins"
+    }
+    else if(choice1 === "paper"){
+        if(choice2 === "rock"){
+            return "Paper Wins";
+        }
+        else if (choice2 === "scissors"){
+            return "Scissors Wins";
+        }
+    }
+    else if(choice1 === "scissors"){
+        if(choice2 === "rock"){
+            return "Rock Wins";
+        }
+        else if(choice === "paper"){
+            return "Scissors Wins";
+        }
+    }
+}
+
+console.log(compare(userChoice,computerChoice));
